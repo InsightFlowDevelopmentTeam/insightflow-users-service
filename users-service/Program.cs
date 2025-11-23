b8b85962cd880f1ff33b89a5e081ce5846ebf0c6
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2025-11-23 12:58:11 -0300

    feat(project): add NuGets and dependencies

--- users-service/Program.cs
+++ users-service/Program.cs
@@ -1,16 +1,21 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 app.Run();
